aaf267d4cccd254f1bfd8432df0ee6ddc5f4c5b6
amr mohamed salah <60838458+NDCHIRO@users.noreply.github.com>
2021-12-17 03:59:56 +0200

    Update 136. Single Number.cpp

--- leetCode/136. Single Number.cpp
+++ leetCode/136. Single Number.cpp
@@ -10,3 +10,32 @@ public:
        
     }
 };
+
+
+//another solution
+/*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // nums.size() == 2 is not a valid test case but i handled it anyway
+        if(nums.size() == 1 || nums.size() == 2)
+            return nums[0];
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(true)
+        {
+            if(i<=0)
+                if(nums[i] != nums[i+1])
+                    return nums[i];
+            
+            if(i >= nums.size()-1)
+                if(nums[i] != nums[i-1])
+                    return nums[i];
+            
+            if(nums[i] != nums[i+1] && nums[i] != nums[i-1])
+                return nums[i];
+            i++;
+        }
+    }
+};
+*/
