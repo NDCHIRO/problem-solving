9daf06d7230ff69a1debb6c9381c772d45cb1b88
amr mohamed salah <60838458+NDCHIRO@users.noreply.github.com>
2022-07-03 23:38:11 +0200

    Update 9. Palindrome Number.java

--- leetCode/9. Palindrome Number.java
+++ leetCode/9. Palindrome Number.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isPalindrome(int x) {
         int reverse=0;
         int reminder=0;
-        int count=1;
         int number=x;
         if(x<0)
             return false;
@@ -11,8 +10,7 @@ class Solution {
         while(number>0)
         {
             reminder=number%10;
-            reverse=reverse*10+reminder;
-            count*=10;
+            reverse= (reverse*10) + reminder;
             number/=10;
         }
         if(x == reverse)
