b63564e2e48fe218f73ce8f5e39a335523b21592
amr mohamed salah <60838458+NDCHIRO@users.noreply.github.com>
2022-07-07 05:11:14 +0200

    Update and rename 206. Reverse Linked List.java to 206. Reverse Linked List iterative.java

--- leetCode/206. Reverse Linked List iterative.java
+++ leetCode/206. Reverse Linked List iterative.java
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//iterative solution
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
