e646b5d5513115b9542863e80d83cef6c1414c29
amr mohamed salah <amrmohamed81298@gmail.com>
2022-09-08 23:22:55 +0200

    Time: 6 ms (96.73%), Space: 284.9 MB (59.48%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,20 +10,6 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-         /*head=head.next;
-     ListNode start=head;
-     while(start!=null){
-	    ListNode end= start;   
-        int sum=0;
-        while(end.val!=0) {
-            sum+= end.val ;
-            end=end.next;
-        }
-        start.val=sum;  
-        start.next=end.next;   
-        start=start.next;    
-	 }
-     return head;*/
         
         int sum=0;
         head=head.next;
@@ -41,7 +27,6 @@ class Solution {
             {
                 slow.val = sum;
                 slow.next=fast.next;              
-                //newList.next = slow;
                 fast = fast.next;
                 if(fast!=null)
                     slow=fast;
