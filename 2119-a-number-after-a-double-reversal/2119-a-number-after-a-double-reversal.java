e2c436d30febb8d166283882d79e4b7af0c8a05b
amr mohamed salah <amrmohamed81298@gmail.com>
2022-10-15 20:40:27 +0200

    Time: 0 ms (100.00%), Space: 41.2 MB (31.26%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        int rev =0;
-        int reminder=0;
         if(num>0 && num%10==0)
             return false;
         return true;
