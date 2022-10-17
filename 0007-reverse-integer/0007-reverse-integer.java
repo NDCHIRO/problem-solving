fbd41c719294fbb2ecaaed7f9211e5b89668ea43
amr mohamed salah <amrmohamed81298@gmail.com>
2022-10-17 18:56:15 +0200

    Time: 2 ms (82.88%), Space: 41.7 MB (30.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -9,9 +9,9 @@ class Solution {
         {
             pop = x%10;
             x=x/10;
-            if((rev>Integer.MAX_VALUE/10) || (rev==Integer.MAX_VALUE/10 && pop>7))
+            if(rev>Integer.MAX_VALUE/10)
                 return 0;
-            if((rev<Integer.MIN_VALUE/10) || (rev==Integer.MIN_VALUE/10 && pop<-8))
+            if(rev<Integer.MIN_VALUE/10)
                 return 0;
             rev = rev*10 + pop;
         }
