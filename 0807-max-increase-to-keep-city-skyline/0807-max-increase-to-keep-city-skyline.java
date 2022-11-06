fd4ecb8aff2e5cfdf88e11ee2c5f5e60cf8353d5
amr mohamed salah <amrmohamed81298@gmail.com>
2022-11-06 16:05:15 +0200

    Time: 1 ms (88.59%), Space: 44.5 MB (28.87%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int[] width = new int[grid.length];
-        int[] height = new int[grid.length];
+        int[] row = new int[grid.length];
+        int[] col = new int[grid.length];
         
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[i].length;j++)
             {
-                width[i] = Math.max(width[i],grid[i][j]);
-                height[i] = Math.max(height[i],grid[j][i]);
+                row[i] = Math.max(row[i],grid[i][j]);
+                col[i] = Math.max(col[i],grid[j][i]);
             }
         }
         
         int score = 0;
         for(int i=0;i<grid.length;i++)
             for(int j=0;j<grid.length;j++)
-                score+= Math.min(width[i],height[j]) - grid[i][j] ;
+                score+= Math.min(row[i],col[j]) - grid[i][j] ;
          
         return score;
-        }                            
+    }                            
 }
