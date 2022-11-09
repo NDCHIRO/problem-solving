f8c26704656267497f46e4ab4a50d68acd765a5f
amr mohamed salah <amrmohamed81298@gmail.com>
2022-11-09 13:21:40 +0200

    Time: 1978 ms (5.03%), Space: 75.4 MB (13.67%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -3,14 +3,14 @@ class StockSpanner {
     int maxSpan;
     public StockSpanner() {
         stock = new ArrayList<Integer>();
-        
+
     }
     
     public int next(int price) {
         maxSpan = 0;
         stock.add(price);
         int i=stock.size()-1;
-        while(i>=0  && stock.get(i)<=stock.get(stock.size()-1))
+        while(i>=0  && stock.get(i)<=stock.get(stock.size()-1) )
         {
             maxSpan+=1;
             i--;
