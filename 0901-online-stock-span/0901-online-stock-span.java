65c0d6a036b76241b89ad55f44e32aafb5616a98
amr mohamed salah <amrmohamed81298@gmail.com>
2022-11-09 13:19:18 +0200

    Time: 2228 ms (5.03%), Space: 75.1 MB (21.90%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,26 @@
+class StockSpanner {
+    ArrayList<Integer> stock;
+    int maxSpan;
+    public StockSpanner() {
+        stock = new ArrayList<Integer>();
+        
+    }
+    
+    public int next(int price) {
+        maxSpan = 0;
+        stock.add(price);
+        int i=stock.size()-1;
+        while(i>=0  && stock.get(i)<=stock.get(stock.size()-1))
+        {
+            maxSpan+=1;
+            i--;
+        }
+        return maxSpan;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
