94f95bb1106174eb8b1a60f42574e48cd0000b96
amr mohamed salah <amrmohamed81298@gmail.com>
2022-11-11 21:23:02 +0200

    Time: 3 ms (60.79%), Space: 42.4 MB (37.63%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -4,24 +4,17 @@ class Solution {
         StringBuilder strT = new StringBuilder();
         
         for(int i=0;i<s.length();i++)
-        {
             if(strS.length()!=0 && s.charAt(i)=='#')
                 strS.deleteCharAt(strS.length()-1);
-            else
-                if(s.charAt(i)!='#')
+            else if(s.charAt(i)!='#')
                     strS.append(s.charAt(i));
-        }
         
         for(int i=0;i<t.length();i++)
-        {
             if(strT.length()!=0 && t.charAt(i)=='#')
                 strT.deleteCharAt(strT.length()-1);
-            else
-                if(t.charAt(i)!='#')
+            else if(t.charAt(i)!='#')
                     strT.append(t.charAt(i));
-        }
-        System.out.println(strS);
-        System.out.println(strT);
+        
         if(strT.toString().equals(strS.toString()))
             return true;
         return false;
