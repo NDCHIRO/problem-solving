298cd1a4489621fe8f44cc10e78e2a3fb3c64da0
amr mohamed salah <amrmohamed81298@gmail.com>
2022-11-28 12:25:54 +0200

    Time: 0 ms (100.00%), Space: 40.3 MB (95.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int len = 0;
-        boolean isSpace = false;
-        int lastChar;
         int i=s.length()-1;
         while(i>=0 && s.charAt(i)== ' ')
-        {
             i--;
-        }
         while(i>=0&&s.charAt(i)!=' ')
         {
             len++;
