f7d6d65c428b3956b385dbf1b1f420feac1447e3
amr mohamed salah <amrmohamed81298@gmail.com>
2023-01-30 16:35:46 +0200

    Time: 0 ms (100.00%), Space: 39.1 MB (70.39%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,5 +1,24 @@
 class Solution {
-    int[] arr = new int[38];
+    //recursive solution
+    int[] dp = new int[38];
+    public int memorize(int n)
+    { 
+        if(n==0) return dp[n]=n;
+        if(n==2||n==1) return dp[n]=1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=memorize(n-1)+memorize(n-2)+memorize(n-3);
+    }
+    public int tribonacci(int n) { 
+        for(int i=0;i<38;i++)
+            dp[i]=-1;
+        return memorize(n);
+    }
+}
+
+/*
+//iterative solution
+int[] arr = new int[38];
     public int tribonacci(int n) { 
         arr[0]=0;
         arr[1]=1;
@@ -10,4 +29,4 @@ class Solution {
         }
         return arr[n];
     }
-}
+*/
