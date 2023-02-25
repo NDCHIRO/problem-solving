bb6179463c171e814fbcaaf8a08fcbcd90f57f5d
amr mohamed salah <amrmohamed81298@gmail.com>
2023-02-25 23:32:51 +0200

    Time: 2 ms (64.08%), Space: 61.2 MB (18.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,8 +4,8 @@ class Solution {
         int maxProfit = -10001;
         for(int i=0;i<prices.length;i++)
         {
-            minPrice = Math.min(minPrice, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice,prices[i]);
+            maxProfit=Math.max(maxProfit,prices[i]-minPrice);
         }
         return maxProfit;
     }
