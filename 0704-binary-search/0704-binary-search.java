568280e29c839bdec2d231cef86d49fc9440933b
amr mohamed salah <amrmohamed81298@gmail.com>
2023-02-27 18:41:45 +0200

    Time: 0 ms (100.00%), Space: 43.1 MB (49.05%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,6 +2,33 @@ class Solution {
     public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length-1;
+        int result = binarySearch(nums,l,r,target);
+        return result;
+    }
+    
+    int binarySearch(int[] nums, int l ,int r,int target)
+    {
+        if(l>r)
+            return -1;
+        int mid = (l+r)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+        {
+            l = mid+1;
+            return binarySearch(nums,l,r,target);
+        }
+        else 
+        {
+            r = mid-1;
+            return binarySearch(nums,l,r,target);
+        }
+    }
+}
+
+/*
+int l = 0;
+        int r = nums.length-1;
         int mid=(l+r)/2;;
         while(r>=l)
         {
@@ -18,5 +45,4 @@ class Solution {
         if(l >= r && nums[mid] != target)
             return -1;
         return mid;
-    }
-}
+*/
