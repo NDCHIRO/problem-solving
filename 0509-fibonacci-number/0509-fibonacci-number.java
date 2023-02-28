0d301771106e0deb32a363eab8a9ae8d55561615
amr mohamed salah <amrmohamed81298@gmail.com>
2023-02-28 20:13:45 +0200

    Time: 0 ms (100.00%), Space: 39.9 MB (18.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,14 @@
 class Solution {
 //iterative solution
-public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+int arr[] = new int[31];
+    public int fib(int n) {
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }   
 }
 /*
