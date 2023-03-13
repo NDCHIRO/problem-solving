5466b60db2e568b51ec973ea939c66c614b0b338
amr mohamed salah <amrmohamed81298@gmail.com>
2023-03-13 18:03:55 +0200

    Time: 0 ms (100.00%), Space: 40.8 MB (38.72%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -21,13 +21,8 @@ class Solution {
     }
     boolean checkSymmetric(TreeNode left , TreeNode right)
     {
-        if(left == null && right == null)
-            return true;
-        else if(left != null && right == null)
-            return false;
-        else if(left == null && right != null)
-            return false;
-        
+        if(left == null || right == null)
+            return left==right;
         else
         {
             if(left.val == right.val)
