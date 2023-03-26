4659411c97b1a0e996c8e8103ec99491b75733ca
amr mohamed salah <amrmohamed81298@gmail.com>
2023-03-26 02:31:26 +0200

    Time: 26 ms (65.88%), Space: 46.5 MB (58.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,4 +1,21 @@
-class Solution {
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
+    }
+}
+/*class Solution {
     public int subarraySum(int[] nums, int k) {
         int sum=0;
         int count=0;
@@ -14,4 +31,4 @@ class Solution {
         }
         return count;
     }
-}
+}*/
