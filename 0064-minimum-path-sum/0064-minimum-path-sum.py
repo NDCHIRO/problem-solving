600537d0343b31994c0867cfd85f769d40ac2169
amr mohamed salah <amrmohamed81298@gmail.com>
2023-03-27 02:51:36 +0200

    Time: 117 ms (21.82%), Space: 20 MB (8.47%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,23 +1,14 @@
-class Solution(object):
-    def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        m, n = len(grid), len(grid[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
 
-        # fill in the first row and column
-        dp[0][0] = grid[0][0]
-        for i in range(1, m):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-        for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-
-        # fill in the rest of the dp array
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
-
-        # return the minimum sum from the top-left corner to the bottom-right corner
-        return dp[m-1][n-1]
+        @cache
+        def dfs(i=0, j=0):
+            if i == m or j == n:
+                return inf
+            if i == m-1 and j == n-1:
+                return grid[i][j]
+            return min(dfs(i+1, j), dfs(i, j+1)) + grid[i][j]
+        
+        return dfs()
