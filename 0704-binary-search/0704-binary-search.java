dacd784d7841ab1bfb9f959cc30dedadaf644f82
amr mohamed salah <amrmohamed81298@gmail.com>
2023-04-05 05:41:56 +0200

    Time: 0 ms (100.00%), Space: 43.9 MB (5.55%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,10 +2,36 @@ class Solution {
     public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length-1;
+        int mid=(l+r)/2;;
+        while(r>=l)
+        {
+            mid = (l+r)/2;
+            /*if(l >= r && nums[mid] != target)
+                return -1;*/
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                r = mid-1;
+            else if(nums[mid] < target)
+                l = mid+1;
+        }
+        if(l >= r && nums[mid] != target)
+            return -1;
+        return mid;
+    }
+    
+    
+}
+
+/*
+    public int search(int[] nums, int target) 
+    {
+        int l = 0;
+        int r = nums.length-1;
         int result = binarySearch(nums,l,r,target);
         return result;
     }
-    
+        
     int binarySearch(int[] nums, int l ,int r,int target)
     {
         if(l>r)
@@ -24,25 +50,4 @@ class Solution {
             return binarySearch(nums,l,r,target);
         }
     }
-}
-
-/*
-int l = 0;
-        int r = nums.length-1;
-        int mid=(l+r)/2;;
-        while(r>=l)
-        {
-            mid = (l+r)/2;
-            if(l >= r && nums[mid] != target)
-                return -1;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                r = mid-1;
-            else if(nums[mid] < target)
-                l = mid+1;
-        }
-        if(l >= r && nums[mid] != target)
-            return -1;
-        return mid;
 */
