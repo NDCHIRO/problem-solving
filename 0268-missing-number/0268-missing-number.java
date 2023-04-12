eb9e375c6c339cc38d2589af22b800f9f6e48d56
amr mohamed salah <amrmohamed81298@gmail.com>
2023-04-12 15:46:41 +0200

    Time: 0 ms (100.00%), Space: 43 MB (71.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,5 +1,18 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int i=0;
+        int xor = 0;
+        for(i=0;i<nums.length;i++)
+        {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
+
+/*
+class Solution {
+    public int missingNumber(int[] nums) {
         Arrays.sort(nums);
         int i=0;
         for(i=0;i<nums.length;i++)
@@ -10,3 +23,4 @@ class Solution {
         return i;
     }
 }
+*/
