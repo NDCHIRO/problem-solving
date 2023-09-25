1663ddaf14a7eedbb79fd30004e079a57fe1a188
amr mohamed salah <amrmohamed81298@gmail.com>
2023-09-25 13:51:44 +0300

    Time: 2 ms (68.98%), Space: 40.9 MB (29.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,10 +1,23 @@
 class Solution {
+    
     public char findTheDifference(String s, String t) {
+        int res=0;
+        for(int i=0;i<s.length();i++)
+            res^=s.charAt(i);
+        for(int i=0;i<t.length();i++)
+            res^=t.charAt(i);
+        
+        return (char)res;
+    }
+}
+
+//solution using frequency array
+    /*public char findTheDifference(String s, String t) {
         int[] freqS=new int[26];
         int[] freqT=new int[26];
         
         for(int i=0;i<s.length();i++)
-            ++freqS[s.charAt(i)-'a'];
+            freqS[s.charAt(i)-'a']++;
         
         for(int i=0;i<t.length();i++)
             freqT[t.charAt(i)-'a']++;
@@ -16,5 +29,4 @@ class Solution {
                 c = (char)(i+'a');
             
         return c;
-    }
-}
+    }*/
