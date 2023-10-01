384a63525d02ccca6ce0cb42c0d312e184eab909
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-01 13:16:38 +0300

    Time: 40 ms (9.60%), Space: 44.7 MB (16.17%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,27 +1,19 @@
 class Solution {
     public String reverseWords(String s) {
-        String s1="";
+        String res="";
         String temp="";
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)==' ')
             {
-                s1+=temp+' ';
+                res+=temp+' ';
                 temp="";
             }
             else
                 temp=s.charAt(i)+temp;
             
         }
-        s1+=temp;
-        return s1;
-    }
-    
-    /*public String reverseChars(String chunkOfString)
-    {
-        String res="";
-        for(int i=0;i<chunkOfString.length();i++)
-            res=chunkOfString.charAt(i)+res;
+        res+=temp;
         return res;
-    }*/
+    }
 }
