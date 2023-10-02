a394c0e8fc465785f8955c366fd9042545045b92
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-02 15:50:34 +0300

    Time: 1600 ms (5.05%), Space: 44 MB (81.16%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
         StringBuilder sb = new StringBuilder();
+        //alice -> player=0 , bob -> player=1
         int player=0;
         sb.append(colors);
         return checkWinner(sb,player);
     }
     public boolean checkWinner(StringBuilder sb,int player)
     {
-        int done=0;
+        //charRemoved is used to check if the player removed a char (charRemoved = true) 
+        //or he didn't remove any char (charRemoved = false) so that means he lost the game
+        boolean charRemoved=false;
         if(sb.length()<3){
-            boolean res=(player==0)?  false:true;
-            return res;
+            return (player==0)?  false:true;
         }
         
         for(int i=1;i<sb.length()-1;i++){
+            //check if the current char = prev char = next char = the current player turn  
             if(sb.charAt(i)==sb.charAt(i+1) && sb.charAt(i)==sb.charAt(i-1)
-              && sb.charAt(i)==((char)('A'+player))){
+              && sb.charAt(i)== ((char)('A'+player)) ){
                 sb.deleteCharAt(i);
+                //change the player's turn between 0 and 1 depends on the current player turn
+                //alice -> player=0 , bob -> player=1
                 player = 1 ^ player;
-                done = 1;
+                charRemoved = true;
             }
         }
-        
-        if(done!=1){
-            boolean res=(player==0)?  false:true;
-            return res;
+        if(charRemoved!=true){
+            return (player==0)?  false:true;
         }
         return checkWinner(sb,player);
     }
