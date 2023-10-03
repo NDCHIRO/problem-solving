befcea7ceaade1052036fd89a78afea09d82271d
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-03 11:55:09 +0300

    Time: 0 ms (100.00%), Space: 39.6 MB (73.19%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,3 +1,18 @@
+//first solution and its better solution
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+       int count = 0;
+        int freq[] = new int[101];
+        for (int i=0;i<nums.length;i++) 
+        {
+            count+=freq[nums[i]];
+            freq[nums[i]]++;
+        }
+        return count;
+    }
+}
+
+//second solution and not the best but its a good idea
 /*class Solution {
     public int numIdenticalPairs(int[] nums) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
@@ -27,13 +42,3 @@
         return sum;
     }
 }*/
-
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-       int count = 0;
-        int freq[] = new int[101];
-        for (int i=0;i<nums.length;i++) 
-            count+=freq[nums[i]]++;    
-        return count;
-    }
-}
