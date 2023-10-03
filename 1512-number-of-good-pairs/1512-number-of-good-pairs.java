779d6bae2592fc7c8f39394027685b22c7e67adf
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-03 12:11:21 +0300

    Time: 1 ms (86.48%), Space: 40.1 MB (26.05%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -18,29 +18,10 @@ class Solution {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         int ans=0;
         for(int i=0;i<nums.length;i++){
-            if(hashMap.containsKey(nums[i]))
-            {
-                ans += hashMap.getOrDefault(nums[i], 0);
-                int val=hashMap.get(nums[i])+1;
-                hashMap.put(nums[i],val);
-            }
-            else
-            {
-                hashMap.put(nums[i],1);
-            }
+            ans += hashMap.getOrDefault(nums[i], 0);
+            //int val=hashMap.get(nums[i])+1;
+            hashMap.put(nums[i],hashMap.getOrDefault(nums[i],0)+1);
         }
-        /*Iterator<Map.Entry<Integer, Integer>> iterator = hashMap.entrySet().iterator();
-
-        // Iterate over the HashMap using a while loop
-        int sum=0;
-        while (iterator.hasNext()) {
-            Map.Entry<Integer, Integer> entry = iterator.next();   
-            if(entry.getValue()>1)
-            {
-                for(int i=1;i<entry.getValue();i++)
-                    sum+=i;
-            }
-        }*/
         return ans;
     }
 }
