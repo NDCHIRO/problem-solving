7ccafc3df657764d2360200b209c6289b3ba6a0c
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-06 18:07:40 +0300

    Time: 0 ms (100.00%), Space: 38.8 MB (93.02%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -12,16 +12,16 @@ class Solution {
 
         if(cache[num]!=null)
             return cache[num];
+        
         int res=0;
         int val=0;
-        int val1=0;
+        
         for(int i=1;i<num;i++)
         {
-            //val = i*DFS(num-i);
             val=Math.max(DFS(num-i),(num-i));
-            //res = Math.max(val,res);
             res = Math.max(val*i,res);
         }
+        
         cache[num]=res;
         return res;
         
