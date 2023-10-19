58519143eb219a39baa76b358abb83250de09773
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-19 21:53:15 +0300

    Time: 6 ms (8.21%), Space: 41.6 MB (6.26%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,24 +1,33 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder strS = new StringBuilder();
-        StringBuilder strT = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        Stack<Character> st1 = new Stack<>();
         
-        s = deleteBackSpace(s);
-        t = deleteBackSpace(t);
+        for (int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='#')
+            {
+                if(!st.empty())
+                st.pop();
+            }
+            else
+                st.push(s.charAt(i));
+        }
         
-        if(s.equals(t))
+        for (int i=0;i<t.length();i++)
+        {
+            if(t.charAt(i)=='#')
+            {
+                if(!st1.empty())  st1.pop();
+            }
+            else
+                st1.push(t.charAt(i));
+        }
+        
+        System.out.println(st.toString());
+        System.out.println(st1.toString());
+        if(st.toString().equals(st1.toString()))
             return true;
         return false;
-    }
-    
-    public String deleteBackSpace(String s)
-    {
-        StringBuilder strS = new StringBuilder();
-        for(int i=0;i<s.length();i++)
-            if(strS.length()!=0 && s.charAt(i)=='#')
-                strS.deleteCharAt(strS.length()-1);
-            else if(s.charAt(i)!='#')
-                    strS.append(s.charAt(i));
-        return strS.toString();
     }
 }
