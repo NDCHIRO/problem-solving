0b21f138fda4be932072482fad9a0fec6ed2dc64
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-20 18:53:07 +0300

    Time: 4 ms (30.87%), Space: 44.7 MB (30.39%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.java
@@ -0,0 +1,28 @@
+public class NestedIterator implements Iterator<Integer> {
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        lists = new Stack<>();
+        lists.push(nestedList.listIterator());
+    }
+
+    public Integer next() {
+        hasNext();
+        return lists.peek().next().getInteger();
+    }
+
+    public boolean hasNext() {
+        while (!lists.empty()) {
+            if (!lists.peek().hasNext()) {
+                lists.pop();
+            } else {
+                NestedInteger x = lists.peek().next();
+                if (x.isInteger())
+                    return lists.peek().previous() == x;
+                lists.push(x.getList().listIterator());
+            }
+        }
+        return false;
+    }
+    
+    private Stack<ListIterator<NestedInteger>> lists;
+}
