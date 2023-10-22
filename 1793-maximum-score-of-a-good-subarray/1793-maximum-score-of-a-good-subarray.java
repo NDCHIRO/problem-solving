b8048bd2a4f97448aa79955f29284b657fc4dc2c
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-23 00:44:00 +0300

    Time: 1126 ms (5.71%), Space: 56.5 MB (76.19%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        int l = k , r = k;
+        int minElement=nums[k];
+        int maxPorduct = nums[k];
+        while(l > 0 || r < nums.length-1)
+        {
+            int left = (l>0)? nums[l-1] : 0;
+            int right = (r<nums.length-1)? nums[r+1] : 0;
+            
+            if(left > right)
+            {
+                l-=1;
+                minElement = Math.min(nums[l],minElement);
+            }
+            else
+            {
+                r+=1;
+                minElement = Math.min(nums[r],minElement);
+            }
+            System.out.println(minElement);
+            maxPorduct = Math.max(maxPorduct,minElement*(r-l+1));
+        }
+        return maxPorduct;
+    }
+}
