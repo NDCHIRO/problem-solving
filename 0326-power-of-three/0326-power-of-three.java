e5296a4479c8bc9c97f3ba4561966642c3701615
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-23 18:15:47 +0300

    Time: 8 ms (75.10%), Space: 43.2 MB (12.74%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return n>0 && Math.pow(3,19) % n == 0;
-        /* failed trial
+        //return n>0 && Math.pow(3,19) % n == 0;
+        // failed trial
         if(n==1)
             return true;
         if(n<=0)
             return false;
-        
-        if((Math.log(n)/ Math.log(3))  == (Math.ceil((Math.log(n))/ Math.log(3))))
+         return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+        /*if((Math.log(n)/ Math.log(3))  == (Math.ceil((Math.log(n))/ Math.log(3))))
             return true;
         return false;*/
     }
