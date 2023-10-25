3f9cb4cd370cf91c917d4f42683098b2bd700e6f
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-25 18:14:25 +0300

    Time: 0 ms (100.00%), Space: 38.8 MB (80.10%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -5,39 +5,8 @@ class Solution {
         int parent = kthGrammar(n-1,(k/2)+(k%2));
         boolean isKOdd = (k%2!=0)? true:false;
         if(parent == 1)
-        {
-            if(isKOdd==true)
-                return 1;
-            else
-                return 0;
-        }
+            return (isKOdd==true)? 1 : 0;
         else
-            if(isKOdd==true)
-                return 0;
-            else
-                return 1;
-        /*String s="0";
-        List<String> list = new ArrayList<String>();
-        list.add(s);
-
-        for(int i=1;i<n;i++)
-        {
-            s="";
-            String index = list.get(list.size()-1);
-            for(char c : index.toCharArray())
-            {
-                if(c=='0')
-                    s+="01";
-                else if(c=='1')
-                    s+="10"; 
-            }
-            //System.out.println(s);
-            list.add(s);
-        }
-
-        
-        char ch = ((list.get(list.size()-1)).charAt(k-1));
-        //System.out.println(ch);
-        return Integer.parseInt(ch+"");*/
+            return (isKOdd==true)? 0 : 1;
     }
 }
