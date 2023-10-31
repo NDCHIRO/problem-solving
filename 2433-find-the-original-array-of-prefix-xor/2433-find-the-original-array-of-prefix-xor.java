2e5f85a5464565ba63b2bdb4e290bc4a944d6132
amr mohamed salah <amrmohamed81298@gmail.com>
2023-10-31 13:01:14 +0200

    Time: 1 ms (100.00%), Space: 58.4 MB (73.59%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+        int[] res = new int[pref.length];
+        int temp=0;
+        res[0]=pref[0];
+        for(int i=1;i<pref.length;i++)
+        {
+            res[i]=pref[i]^pref[i-1];
+        }
+        return res;
+    }
+}
