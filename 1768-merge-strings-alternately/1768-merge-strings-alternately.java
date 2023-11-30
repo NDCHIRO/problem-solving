6fca6e78b1a15c7401372472ed4ca5f06ce6de80
amr mohamed salah <amrmohamed81298@gmail.com>
2023-11-30 18:39:45 +0200

    Time: 7 ms (24.78%), Space: 42.4 MB (7.46%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -4,7 +4,17 @@ class Solution {
         int s2Len = word2.length();
         int minLen = s1Len<=s2Len? s1Len: s2Len;
         String merge = "";
-        for(int i=0;i<minLen;i++)
+        int i=0,j=0;
+        while(i<s1Len || j<s2Len)
+        {
+            if(i<s1Len)
+                merge+=word1.charAt(i++);
+            if(j<s2Len)
+                merge+=word2.charAt(j++);
+        }
+        return merge;
+    }
+        /*for(int i=0;i<minLen;i++)
         {
             merge+=word1.charAt(i);
             merge+=word2.charAt(i);
@@ -18,5 +28,5 @@ class Solution {
             for(int i=minLen;i<s2Len;i++)
                 merge+=word2.charAt(i);
         return merge;
-    }
+    }*/
 }
