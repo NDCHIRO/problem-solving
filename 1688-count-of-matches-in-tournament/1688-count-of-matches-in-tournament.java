7a64e89573f95d5035642c094361f87932c4bb40
amr mohamed salah <amrmohamed81298@gmail.com>
2023-12-05 11:28:23 +0200

    Time: 0 ms (100.00%), Space: 39.3 MB (40.19%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,13 +1,14 @@
 class Solution {
     int noOfMatches=0;
     public int numberOfMatches(int n) {
-        if(n==1)
+        return n-1;
+        /*if(n==1)
             return noOfMatches;
          
         if(n%2==0)
             return (noOfMatches+n/2)+numberOfMatches(n/2);
         else        
-            return (1+(noOfMatches+n/2))+numberOfMatches(n/2);
+            return (1+(noOfMatches+n/2))+numberOfMatches(n/2);*/
     }
     
 }
