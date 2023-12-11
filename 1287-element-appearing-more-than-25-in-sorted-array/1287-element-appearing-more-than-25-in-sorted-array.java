4fe85959bd502ab3d96861318a441dbce51ab298
amr mohamed salah <amrmohamed81298@gmail.com>
2023-12-11 14:39:08 +0200

    Time: 4 ms (36.40%), Space: 43.8 MB (31.80%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -1,17 +1,6 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++)
-             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-    
-        int target = arr.length / 4;
-         for (int key : map.keySet()) 
-            if (map.get(key) > target) 
-                return key;
-            
-        
-        return -1;
-        /*int length = arr.length;
+        int length = arr.length;
         int[] freq = new int[100001];
         for(int i=0;i<arr.length;i++)
         {
@@ -26,6 +15,19 @@ class Solution {
             max = Math.max(max,freq[i]);
             
         }
-        return index;*/
+        return index;
+        //using hashmap
+        /*HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+    
+        int target = arr.length / 4;
+         for (int key : map.keySet()) 
+            if (map.get(key) > target) 
+                return key;
+            
+        
+        return -1;*/
+        
     }
 }
