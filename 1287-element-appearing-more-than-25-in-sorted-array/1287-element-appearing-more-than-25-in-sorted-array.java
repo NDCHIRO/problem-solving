5487c2709c84e0fd65b7fce8ad2a5f87bd94d8c7
amr mohamed salah <amrmohamed81298@gmail.com>
2023-12-11 14:35:58 +0200

    Time: 11 ms (24.41%), Space: 45.3 MB (5.58%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -3,14 +3,7 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<arr.length;i++)
         {
-            if(map.get(arr[i])==null)
-            {
-                map.put(arr[i],1);
-            }
-            else
-            {
-                map.put(arr[i],map.get(arr[i])+1);
-            }
+             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
         int target = arr.length / 4;
          for (int key : map.keySet()) {
