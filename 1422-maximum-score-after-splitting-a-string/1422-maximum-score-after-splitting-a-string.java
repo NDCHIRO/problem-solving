695a6e77f2a91675f13d32ebee13b14d94424dc0
amr mohamed salah <amrmohamed81298@gmail.com>
2023-12-22 18:24:32 +0200

    Time: 10 ms (27.11%), Space: 44.5 MB (6.22%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -4,33 +4,34 @@ class Solution {
         String left = "";
         int maxSum = 0;
         int localSum = 0;
+        
         if(s.length()==2)
         {
-            maxSum+=s.charAt(0)=='0'?1:0;
-            maxSum+=s.charAt(1)=='1'?1:0;
+            maxSum+= s.charAt(0)=='0'? 1 : 0;
+            maxSum+= s.charAt(1)=='1'? 1 : 0;
             return maxSum;
         }
+        
         for(int i=1;i<s.length();i++)
         {
             right=s.substring(i,s.length());
             left = s.substring(0,i);
             int sumZeros=0;
             int sumOnes=0;
+            
             for(int j=0;j<right.length();j++)
-            {
                 if(right.charAt(j)=='1')
                     sumOnes+=1;
-            }
+
             for(int j=0;j<left.length();j++)
-            {
                 if(left.charAt(j)=='0')
                     sumZeros+=1;
-            }
 
             localSum=sumZeros+sumOnes;
             maxSum = Math.max(localSum,maxSum);    
 
         }
+        
         return maxSum;
     }
 }
