533c807b590147d34829f17f83c99faa531fa627
amr mohamed salah <amrmohamed81298@gmail.com>
2024-01-05 23:19:06 +0200

    Time: 259 ms (11.38%), Space: 43.9 MB (40.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,25 @@
 class Solution {
-    int []memo;
+    int[] memo;
     public int lengthOfLIS(int[] nums) {
-        memo = new int[2502];
+        memo = new int[2501];
         for(int i=0;i<memo.length;i++)
             memo[i]=-1;
-        return getLongestSubsequence(nums,0,-1);
+        return getLongestSubsequance(nums,-1,0);
     }
     
-    public int getLongestSubsequence(int nums[], int i,int prev){
+    int getLongestSubsequance(int[] nums, int prev , int i)
+    {
         if(i>=nums.length)
             return 0;
         if(memo[prev+1]!=-1)
             return memo[prev+1];
-        int dontTake = getLongestSubsequence(nums,i+1,prev);
+        
+        int dontTake = getLongestSubsequance(nums,prev,i+1);
         int take = 0;
-        if(prev == -1 || nums[i] > nums[prev])
-            take = 1+getLongestSubsequence(nums,i+1,i);
+        if(prev==-1 || nums[prev]<nums[i])
+            take = 1 + getLongestSubsequance(nums,i,i+1);
         memo[prev+1] = Math.max(take,dontTake);
         return memo[prev+1];
-        //totalSum = Math.max(totalSum,Sum);
+        
     }
 }
