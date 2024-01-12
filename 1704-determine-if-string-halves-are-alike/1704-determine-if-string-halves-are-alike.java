4d254366a2f0beda784edff732cb45dd72427ae7
amr mohamed salah <amrmohamed81298@gmail.com>
2024-01-13 01:50:43 +0200

    Time: 3 ms (88.21%), Space: 41.8 MB (24.63%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,24 +3,23 @@ class Solution {
         String s1 = s.substring(0,s.length()/2);
         String s2 = s.substring(s.length()/2,s.length());
         
-        int count1=0;
-        s1=s1.toLowerCase();
-        for(int i=0;i<s1.length();i++)
-        {
-            if(s1.charAt(i) == 'a' || s1.charAt(i) == 'e' ||s1.charAt(i) == 'i' || s1.charAt(i) == 'o'                      || s1.charAt(i) == 'u')    
-                count1++;
-        }
-        
-        int count2=0;
-        s2=s2.toLowerCase();
-        for(int i=0;i<s2.length();i++)
-        {
-            if(s2.charAt(i) == 'a' || s2.charAt(i) == 'e' ||s2.charAt(i) == 'i' || s2.charAt(i) == 'o'                      || s2.charAt(i) == 'u')    
-                count2++;
-        }
+        int count1=convert(s1);
+        int count2=convert(s2);
         
         if(count1 == count2)
             return true;
         return false;
+    }
+    
+    int convert(String s)
+    {
+        int count=0;
+        s=s.toLowerCase();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' ||s.charAt(i) == 'i' || s.charAt(i) == 'o'                      || s.charAt(i) == 'u')    
+                count++;
+        }
+        return count;
     }
 }
