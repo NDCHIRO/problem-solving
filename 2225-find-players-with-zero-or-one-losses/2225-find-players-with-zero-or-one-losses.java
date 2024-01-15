5676a215b2b4e47c1f6636b8e70c5f17b7c33538
amr mohamed salah <amrmohamed81298@gmail.com>
2024-01-15 22:14:34 +0200

    Time: 113 ms (24.50%), Space: 110.4 MB (11.61%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -12,50 +12,51 @@ class Solution {
                     list.set(j,list.get(j)+1);
                     map.put(matches[i][j],list);
                 }
-            else
-            {
-                ArrayList<Integer> arrList = new ArrayList<>();
-                if(j==0)
-                {
-                    arrList.add(1);
-                    arrList.add(0);
-                }
+                
                 else
                 {
-                    arrList.add(0);
-                    arrList.add(1);
+                    ArrayList<Integer> arrList = new ArrayList<>();
+                    if(j==0)
+                    {
+                        arrList.add(1);
+                        arrList.add(0);
+                    }
+                    else
+                    {
+                        arrList.add(0);
+                        arrList.add(1);
+                    }
+                    map.put(matches[i][j],arrList);
                 }
-                map.put(matches[i][j],arrList);
-            }
             }
                 
         }
         
-        //System.out.println(map);
         
-        List<List<Integer>> arr = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         ArrayList<Integer> inner1 = new ArrayList<>();
         ArrayList<Integer> inner2 = new ArrayList<>();
+        
         for(Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
             int key = entry.getKey();
             ArrayList<Integer> values = entry.getValue();
             
             
             if(values.get(0)>0 && values.get(1)==0)
-            {
                 inner1.add(key);
-            }
+
             else if(values.get(0)>=0 && values.get(1)==1)
-            {
                 inner2.add(key);
-            }
+            
         }
+        
         Collections.sort(inner1);
         Collections.sort(inner2);
-        arr.add(inner1);
-        arr.add(inner2);
         
-        return arr;
+        res.add(inner1);
+        res.add(inner2);
+        
+        return res;
         
     }
 }
