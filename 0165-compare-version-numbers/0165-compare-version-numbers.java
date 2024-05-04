972f432a3dc413b1d0ad727dc19ed0e898a6ffce
amr mohamed salah <amrmohamed81298@gmail.com>
2024-05-04 06:03:04 +0300

    Time: 1 ms (78.70%), Space: 41.6 MB (23.52%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,7 +2,6 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] arrV1 = version1.split("\\.");
         String[] arrV2 = version2.split("\\.");
-        int res=-2;
         int maxLength = (arrV1.length>arrV2.length)? arrV1.length:arrV2.length;
         for(int i=0 ; i<maxLength;i++)
         {
@@ -15,17 +14,5 @@ class Solution {
             return -1;
         }
         return 0;
-    }
-    
-    int splitWithDot(String v1, String v2)
-    {
-        int numberV1 = Integer.parseInt(v1);
-        int numberV2 = Integer.parseInt(v2);
-        if(numberV1 > numberV2)
-            return 1;
-        else if(numberV2 > numberV1)
-            return -1;
-        else
-            return 0;
     }
 }
