5836160ce03e092316d1f5085ea591b87762e55b
amr mohamed salah <amrmohamed81298@gmail.com>
2024-10-08 21:13:02 +0300

    Time: 37 ms (34.86%), Space: 54.8 MB (60.00%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -7,11 +7,12 @@ class Solution {
             if(s.charAt(i) == '[')
                 list.add(s.charAt(i));
             else
+            {
                 if(!list.isEmpty()) 
                     list.remove(list.size()-1);
                 else
                     count++;
-        
+            }
         return (count+ 1) / 2;
     }
 }
