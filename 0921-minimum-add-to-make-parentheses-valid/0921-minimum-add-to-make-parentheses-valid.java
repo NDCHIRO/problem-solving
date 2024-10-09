431f5342b19fb9e0df9d6e35a830f3ddba66f5bd
amr mohamed salah <amrmohamed81298@gmail.com>
2024-10-09 21:48:39 +0300

    Time: 1 ms (58.43%), Space: 41.8 MB (8.99%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -6,9 +6,12 @@ class Solution {
         {
             if(s.charAt(i)=='(')
                 stack.push('(');
-            else
-                if(!stack.isEmpty())stack.pop();
-                else count++;
+            else{
+                if(!stack.isEmpty())
+                    stack.pop();
+                else
+                    count++;
+            }
             
         }
         return count + stack.size();
