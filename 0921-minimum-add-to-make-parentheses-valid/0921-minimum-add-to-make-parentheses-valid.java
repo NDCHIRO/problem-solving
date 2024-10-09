1a7ac95e0d9d3c41e2d718da7e8a6f48b3633efb
amr mohamed salah <amrmohamed81298@gmail.com>
2024-10-09 21:47:49 +0300

    Time: 1 ms (58.43%), Space: 41.7 MB (16.86%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+                stack.push('(');
+            else
+                if(!stack.isEmpty())stack.pop();
+                else count++;
+            
+        }
+        return count + stack.size();
+    }
+}
