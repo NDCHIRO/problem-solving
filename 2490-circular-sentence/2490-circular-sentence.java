d812ddb13ac326d5729b09495bdb733d50167b00
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-02 14:27:49 +0200

    Time: 1 ms (96.81%), Space: 41.7 MB (90.50%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -3,13 +3,9 @@ class Solution {
         if(sentence.charAt(0)!=sentence.charAt(sentence.length()-1))
             return false;
         
-       char lastChar=' ';
        for(int i=0;i<sentence.length()-1;i++)
        {
-           if(sentence.charAt(i+1) == ' ')
-               lastChar = sentence.charAt(i);
-           if(sentence.charAt(i) == ' ')
-               if(sentence.charAt(i+1) != lastChar)
+           if(sentence.charAt(i) == ' '&& sentence.charAt(i+1) != sentence.charAt(i-1))
                    return false;
        }
        return true;    
