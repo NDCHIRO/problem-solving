cf9aba4575f2b0d80f76079cbbc2d67dafb0f7ce
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-02 15:16:15 +0200

    Time: 2 ms (18.09%), Space: 41.7 MB (90.50%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
        for(int i=0;i<sentence.length()-1;i++)
-       {
            if(sentence.charAt(i) == ' '&& sentence.charAt(i+1) != sentence.charAt(i-1))
                    return false;
-       }
        return sentence.charAt(0)==sentence.charAt(sentence.length()-1);    
     }
 }
