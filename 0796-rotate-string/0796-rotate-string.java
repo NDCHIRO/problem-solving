202c9f107b4cbac817ac82f4363b27120a18e407
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-04 01:29:58 +0200

    Time: 0 ms (100.00%), Space: 41.3 MB (66.58%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public boolean rotateString(String s, String goal) {
+        // Check if the lengths are different
+        if (s.length() != goal.length()) return false;
+
+        // Create a new string by concatenating 's' with itself
+        String doubledString = s + s;
+
+        // Use contains to search for 'goal' in 'doubledString'
+        // If contains return true, 'goal' is a substring
+        return doubledString.contains(goal);
+    }
+}
