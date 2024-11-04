9f7253fb42bd53f394800eba7a7d3db531d082aa
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-04 21:13:22 +0200

    Time: 22 ms (46.61%), Space: 45.6 MB (87.71%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -4,13 +4,10 @@ class Solution {
         int count = 1; // Start with a count of 1 for the first character
 
         for (int i = 1; i <= word.length(); i++) {
-            // Check if we're at the end of the string or the current character is different from the previous one
             if (i == word.length() || word.charAt(i) != word.charAt(i - 1) || count == 9) {
-                // Append the count and character
                 comp.append(count).append(word.charAt(i - 1));
                 count = 1; // Reset count for the new sequence
             } else {
-                // Increment count if the character is the same as the previous one
                 count++;
             }
         }
