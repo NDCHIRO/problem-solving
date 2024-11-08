4fce4367ed54020d8ca804605c7ba5302fa688f4
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-08 17:17:11 +0200

    Time: 10 ms (6.47%), Space: 42.8 MB (9.91%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -2,13 +2,14 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int i= 0;
         String res = "";
-        for(i=0;i<Math.min(word1.length(),word2.length()) ; i++)
-        {
-            res+=word1.charAt(i);
-            res+=word2.charAt(i);
-        }
-        for(;i<Math.max(word1.length(),word2.length()) ; i++)
-            res += (Math.max(word1.length(),word2.length()) == word1.length()?  word1.charAt(i):                                word2.charAt(i));
+        int minLength = Math.min(word1.length(),word2.length());
+        int maxLength = Math.max(word1.length(),word2.length());
+        
+        for(i=0;i<minLength ; i++)
+            res+= (word1.charAt(i)+""+word2.charAt(i));
+        
+        for( ;i<maxLength; i++)
+            res += ( maxLength == word1.length()?  word1.charAt(i):word2.charAt(i) );
                     
         return res;
     }
