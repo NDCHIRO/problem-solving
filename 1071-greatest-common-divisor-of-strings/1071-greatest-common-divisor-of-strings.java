4c115bdc2b501040e57169abd79056a83407e208
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-09 13:37:04 +0200

    Time: 1 ms (67.42%), Space: 42 MB (77.19%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,20 +1,24 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length();
-        int len2 = str2.length();
-    
-        for(int i=Math.min(len1,len2); i>=1;i--)
+        int len1= str1.length();
+        int len2= str2.length();
+        int minLen = len1>len2? len2:len1;
+        boolean res = false;
+        for(int i=minLen; i>=1;i--)
         {
-            if(check(str1,str2,i))
-                return str1.substring(0, i);
+            if(valid(str1,str2,i))
+                return str1.substring(0,i);
         }
+        
         return "";
     }
     
-    private boolean check(String str1, String str2, int k){
-        if(str1.length()%k !=0 || str2.length() % k !=0 )
+    private boolean valid(String str1, String str2, int k)
+    {
+        if(str1.length()%k !=0 || str2.length()%k !=0)
             return false;
-        String base = str1.substring(0,k);
+        
+        String base = str1.substring(0, k);
         return str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty();
     }
 }
