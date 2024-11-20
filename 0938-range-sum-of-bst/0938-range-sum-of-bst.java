161cfff09eebfcba0a1793f136f3c8a596cd104e
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-20 21:31:15 +0200

    Time: 1 ms (45.93%), Space: 51.1 MB (63.85%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -14,15 +14,15 @@
  * }
  */
 class Solution {
-    int sum=0;
+    int sum = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null)
             return 0;
-        if(root.val >=low && root.val <= high)
+        if(root.val >= low && root.val <= high)
             sum+=root.val;
+        
         rangeSumBST(root.left,low,high);
         rangeSumBST(root.right,low,high);
-        
         return sum;
     }
 }
