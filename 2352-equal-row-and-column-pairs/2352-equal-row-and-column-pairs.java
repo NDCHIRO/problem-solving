1b984fcaea003a7cc83f3c35b09e791ce6c5d0d3
amr mohamed salah <amrmohamed81298@gmail.com>
2024-11-30 16:04:38 +0200

    Time: 180 ms (5.86%), Space: 52.6 MB (27.07%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,26 +1,24 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         List<String> rows = new ArrayList<>();
+        
         for(int i=0;i<grid.length;i++)
         {
             StringBuilder builder = new StringBuilder();
+            
             for(int j=0;j<grid.length;j++)
-            {
                 builder.append(grid[i][j]).append(",");
-            }
+        
             rows.add(builder.toString());
         }
         
-        System.out.println(rows);
+        int count = 0;
         
-        int count =0;
         for(int i=0;i<grid.length;i++)
         {
             StringBuilder builder = new StringBuilder();
             for(int j=0;j<grid.length;j++)
-            {
                 builder.append(grid[j][i]).append(",");
-            }
             
             for(int j=0;j<rows.size();j++)
                 if(rows.get(j).equals(builder.toString()))
